d5497500ec49c95637078ce1c2fed9d7a9566aa9
Nick123171 <147677170+Nick123171@users.noreply.github.com>
2023-10-23 17:47:38 -0500

    Add files via upload

--- Coding-Excercises/Substring Comparisons/Solution/src/main/java/com/mycompany/solution/Solution.java
+++ Coding-Excercises/Substring Comparisons/Solution/src/main/java/com/mycompany/solution/Solution.java
@@ -20,8 +20,24 @@ public class Solution {
     }
     
     public static String getSmallestAndLargest(String s, int k){
-        //write your code here    
-        return s;    
+        //write your code here
+        String highestString = "";
+        String lowestString = "zzz";
+        int leng = (s.length() - k);
+        for (int i = 0; i <= leng; i++) {
+            String substring = s.substring(i, i + k);
+            int hol = substring.compareTo(highestString);
+            if (hol > 0) {
+                highestString = substring;
+            }
+            hol = substring.compareTo(lowestString);
+            if (hol < 0) {
+                lowestString = substring;
+            }
+        }
+        String end = lowestString + "\n" + highestString;
+
+        return end;    
     }
     
     
